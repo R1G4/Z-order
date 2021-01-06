8455cf2b3f8cd044aa021c2e9091458f02e7ba9c
Hangoms <huny0524@gmail.com>
2021-01-06 23:10:36 +0900

    버그 해결
    해결해결

--- R1G4 Sub Team Portfolio/playGround.cpp
+++ R1G4 Sub Team Portfolio/playGround.cpp
@@ -12,6 +12,7 @@ playGround::~playGround()
 
 HRESULT playGround::init()
 {
+	gameNode::init(true);
 	SCENEMANAGER->addScene("시작화면", new StartScene);
 	SCENEMANAGER->addScene("세이브로드", new saveLoad);
 	SCENEMANAGER->changeScene("시작화면");
@@ -40,7 +41,7 @@ void playGround::render()
 	PatBlt(getMemDC(), 0, 0, WINSIZEX, WINSIZEY, WHITENESS);
 	//================ 위에 건들지 마라 ==============================
 	SCENEMANAGER->render();
-	//TIMEMANAGER->render(getMemDC());
+	TIMEMANAGER->render(getMemDC());
 	//================= 아래도 건들지 마라 ==============================
 	if(!VIDEOMANAGER->checkPlay())_backBuffer->render(getHDC());
 }
