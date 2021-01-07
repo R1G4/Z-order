d009160781c1baec5272cb049c15f831b53dd288
qkrwhd0637 <76798298+qkrwhd0637@users.noreply.github.com>
2021-01-07 14:14:59 +0900

    에너미 테스트 추가
    스쿨 걸 한명 패턴
    총제적으로 애니메이션 에러있음 존재

---
+++ 에너미 테스트/playGround.cpp
@@ -0,0 +1,61 @@
+#include "stdafx.h"
+#include "playGround.h"
+
+playGround::playGround()
+{
+}
+
+
+playGround::~playGround()
+{
+}
+
+HRESULT playGround::init()
+{
+	gameNode::init(true);
+
+	IMAGEMANAGER->addImage("새배경", "0.bmp", WINSIZEX, WINSIZEY, true, RGB(255, 0, 255));
+
+	
+
+	SCENEMANAGER->addScene("테스트씬", new testScene);
+	SCENEMANAGER->addScene("에너미", new enemyManager);
+
+	SCENEMANAGER->changeScene("에너미");
+	//_em->init();
+	return S_OK;
+}
+
+
+void playGround::release()
+{
+	gameNode::release();
+	//_em->release();
+	
+}
+
+void playGround::update()
+{
+	gameNode::update();
+	//_em->update();
+	SCENEMANAGER->update();
+	
+}
+
+
+void playGround::render()
+{
+	PatBlt(getMemDC(), 0, 0, WINSIZEX, WINSIZEY, WHITENESS);
+	//================ 위에 건들지 마라 ==============================
+
+	IMAGEMANAGER->findImage("새배경")->render(getMemDC());
+	
+	
+	SCENEMANAGER->render();
+
+	//_em->update();
+	TIMEMANAGER->render(getMemDC());
+	//================= 아래도 건들지 마라 ==============================
+	_backBuffer->render(getHDC());
+}
+
