6243d626e863ea1f2409857b3b9657549dec6b24
PSH <zzana0219@naver.com>
2021-01-08 15:16:17 +0900

    리셋

--- R1G4 Sub Team Portfolio temp/picCollision.cpp
+++
@@ -1,7 +0,0 @@
-#include"stdafx.h"
-#include"tempStage.h"
-void tempStage ::picCollision()
-{
-	probeX = (_player->getShadow().left + _player->getShadow().right) / 2;
-	probeY = (_player->getShadow().top + _player->getShadow().bottom) / 2;
-}
