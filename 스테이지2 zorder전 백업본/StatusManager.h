f46fbb0f6e250c4841458611fd3dfe0c43efeaa7
mmmdong <76802602+mmmdong@users.noreply.github.com>
2021-01-11 14:11:42 +0900

    sdfsdf

--- 스테이지2 zorder전 백업본/StatusManager.h
+++ 스테이지2 zorder전 백업본/StatusManager.h
@@ -1,23 +1,57 @@
 #pragma once
-struct HPBar {
-	int hp;
-	image* img;
-};
-class StatusManager :
-	public singletonBase<StatusManager>
+#include "singletonBase.h"
+#include <map>
+
+#include "inc/fmod.hpp"
+#pragma comment(lib, "lib/fmodex64_vc.lib")
+
+using namespace FMOD;
+
+#define SOUNDBUFFER 10
+#define EXTRACHANNELBUFFER 5
+
+#define TOTALSOUNDBUFFER SOUNDBUFFER + EXTRACHANNELBUFFER
+
+
+class soundManager : public singletonBase<soundManager>
 {
 private:
-	int HP;
-	int mahaGauge;
-	vector<HPBar*> _vHPBar;
-	vector<HPBar*>::iterator _viHPBar;
+	typedef map<string, Sound**>			arrSounds;
+	typedef map<string, Sound**>::iterator	arrSoundsIter;
+
+	typedef map<string, Channel**>				arrChannels;
+	typedef map<string, Channel**>::iterator	arrChannelIter;
+
+private:
+	System*			_system;
+	Sound**			_sound;
+	Channel**		_channel;
+
+	arrSounds		_mTotalSounds;
+
 public:
-	void setHpBar(string strkey);
-	int getHp() { return HP; }
-	void setHp(int Hp) { HP = Hp; }
-	vector<HPBar*> getHpBar() { return _vHPBar; }
-	vector<HPBar*>::iterator getHpBarIter() { return _viHPBar; }
-	void damaged(int damage);
-	void heal(int heal,string strkey);
+	soundManager();
+	~soundManager();
+
+	HRESULT init();
+	void release();
+	void update();
+
+	//   사운드 추가함수(키 값, 음악파일명, 브금or효과음, 루프할껀지)
+	void addSound(string keyName, string soundName, bool bgm, bool loop);
+
+	//                             0.0f ~ 1.0f -> 0 ~ 255
+	void play(string keyName, float volume = 1.0f);
+	void stop(string keyName);
+	void pause(string keyName);
+	void resume(string keyName);
+	void setVolume(float volume);
+	void setPoint(string keyName, float portion);
+	float getPortion(string keyName);
+
+
+	//호옥시이 몰라서 만들어둠
+	bool isPlaySound(string keyName);
+	bool isPauseSound(string keyName);
 };
 
